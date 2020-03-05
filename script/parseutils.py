f217ee27b5bdfa8be1788af9a609a5530423be72
wutuo <wt05001@163.com>
2020-03-06 07:33:30 +0800

    项目初始化

---
+++ script/parseutils.py
@@ -0,0 +1,126 @@
+import json
+import time
+import urllib.parse
+from urllib.parse import urljoin
+
+from lxml import etree
+from selenium import webdriver
+
+# 将Chrome设置置成不加载图片的无界面运行状态
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+chrome_options.add_argument("--headless")
+chrome_path = r'C:\Program Files\Python37\chromedriver.exe'
+
+
+class BaseSpider(object):
+    def parse_page(self):
+        pass
+
+    def parse_url(self):
+        pass
+
+    def save(self, data):
+        pass
+
+    @staticmethod
+    def get_page_content(url):
+        '''
+            1、百度页面异步js加载，用requests直接获取会丢失页面数据，采用selenium驱动页面
+            2、每次请求sleep 3秒，防止百度识别是爬虫请求
+        :param url:
+        :return:
+        '''
+        time.sleep(3)
+
+        driver = webdriver.Chrome(chrome_path, chrome_options=chrome_options)
+        driver.get(url)
+        return driver.page_source
+
+
+class BaiduSpider(BaseSpider):
+    def __init__(self, keyword, page_num):
+        self.desc = "Baidu"
+        self.keyword = str(keyword)
+        self.page_num = page_num
+        self.url = "https://www.baidu.com/s?" \
+                   + urllib.parse.urlencode({'wd': self.keyword}) + "&" + urllib.parse.urlencode({'oq': self.keyword})
+
+    def parse_page(self):
+        for i in range(1, int(self.page_num) + 1):
+            print("正在爬取第{}页....{}".format(i, self.url))
+            title = ""
+            abstract = ""
+            flag = 11
+            html = BaseSpider.get_page_content(self.url)
+            content = etree.HTML(html)
+            print(content)
+            for j in range(1, flag):
+                data = {}
+                # 标题
+                res_title = content.xpath('//*[@id="%d"]/h3/a' % ((i - 1) * 10 + j))
+                if res_title:
+                    title = res_title[0].xpath('string(.)')
+
+                # URL
+                sub_url = content.xpath('//*[@id="%d"]/h3/a/@href' % ((i - 1) * 10 + j))
+                if sub_url:
+                    sub_url = sub_url[0]
+
+                # 描述
+                res_abstract = content.xpath('//*[@id="%d"]/div[@class="c-abstract"]' % ((i - 1) * 10 + j)) \
+                               or content.xpath('//*[@id="%d"]/div[@class="c-abstract c-abstract-en"]' % ((i - 1) * 10 + j))
+                if res_abstract:
+                    abstract = res_abstract[0].xpath('string(.)')
+                else:
+                    res_abstract = content.xpath('//*[@id="%d"]/div/div[2]/div[@class="c-abstract"]' % ((i - 1) * 10 + j)) \
+                                   or content.xpath('//*[@id="%d"]/div/div[2]/div[@class="c-abstract c-abstract-en"]' % ((i - 1) * 10 + j))
+                    if res_abstract:
+                        abstract = res_abstract[0].xpath('string(.)')
+
+                data['title'] = title
+                data['sub_url'] = sub_url
+                data['abstract'] = abstract
+
+                next_page_index = flag - 1 if i == 1 else flag
+                rel_url = content.xpath('//*[@id="page"]/a[{}]/@href'.format(next_page_index))
+                if rel_url:
+                    self.url = urljoin(self.url, rel_url[0])
+                else:
+                    print("～～～无更多页面数据～～～")
+                    return
+                yield data
+
+    def save(self, data):
+        file = open(self.desc + "_data.json", 'w+', encoding='utf-8')
+        for result in data:
+            print(result)
+            file.write(json.dumps(result, indent=2, ensure_ascii=False))
+
+
+class BingSpider(BaseSpider):
+    def __init__(self, keyword, page_num):
+        self.desc = "Bing"
+        self.url = ""
+        self.keyword = keyword
+        self.page_num = page_num
+
+    def parse_page(self):
+        pass
+
+    def parse_url(self):
+        pass
+
+
+class GoogleSpider(BaseSpider):
+    def __init__(self, keyword, page_num):
+        self.desc = "Google"
+        self.url = ""
+        self.keyword = keyword
+        self.page_num = page_num
+
+    def parse_page(self):
+        pass
+
+    def parse_url(self):
+        pass
---
+++ spider.py
@@ -0,0 +1,35 @@
+from script.parseutils import BaiduSpider
+from script.parseutils import BingSpider
+from script.parseutils import GoogleSpider
+
+if __name__ == '__main__':
+    search_engine = input("选择搜索引擎[1:Baidu 2:Bing 3:Google]:")
+    keyword = input("搜索关键字:")
+    page_num = input("搜索页数:")
+
+    while 1:
+        if page_num.isdigit():
+            break
+        else:
+            print("页码错误", end=", ")
+            page_num = input("请重新输入搜索页数:")
+
+    while 1:
+        if not page_num.isdigit():
+            search_engine = input("不支持的搜索引擎,请重新选择搜索引擎[1:Baidu 2:Bing 3:Google]:")
+            continue
+
+        if int(search_engine) == 1:
+            search = BaiduSpider(keyword, page_num)
+            break
+        elif int(search_engine) == 2:
+            search = BingSpider(keyword, page_num)
+            break
+        elif int(search_engine) == 3:
+            search = GoogleSpider(keyword, page_num)
+            break
+        else:
+            search_engine = input("不支持的搜索引擎,请重新选择搜索引擎[1:Baidu 2:Bing 3:Google]:")
+
+    results = search.parse_page()
+    search.save(results)
